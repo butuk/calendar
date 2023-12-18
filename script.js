05f8e62adf6bdfd056e3770efe411901d353d9b2
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-12-18 11:10:04 +0100

    refactor: change the slide

--- script.js
+++ script.js
@@ -24,13 +24,22 @@ const year = new Year();
 // Layout creation
 const slides = document.querySelector(".slides");
 for (let i = 0; i < 3; i++) {
+  const header = createElement("section", "header");
+  const logo = createElement("div", "logo");
+  logo.classList.add("day");
+  logo.textContent = "2024";
   const slide = createElement("section", "slide");
   const hr = createElement("section", "hr");
-  head.renderIn(hr, 31, 3);
   const calendar = createElement("section", "grid");
+
+  head.renderIn(hr, 31, 3);
+
   year.createDefault();
   year.switchTo(country);
   year.renderIn(calendar, 31, 3, language);
+
+  header.append(logo);
+  slide.append(header);
   slide.append(hr);
   slide.append(calendar);
   slides.append(slide);
@@ -97,130 +106,39 @@ languages.addEventListener("click", (e) => {
 });
 
 // Slider
-/*const left = document.querySelector('.left');
-const right = document.querySelector('.right');
+let isDragging = false;
+let offsetX;
 
-left.addEventListener('click', goLeft)
-right.addEventListener('click', goRight);*/
 
-document.addEventListener("animationend", () => {
-  slides.classList.remove("toLeft");
-  slides.classList.remove("toRight");
-  addListeners();
-});
+document.addEventListener("mousedown", handleTouchStart);
+document.addEventListener("touchstart", handleTouchStart);
 
-document.addEventListener('animationstart', () => {
-  removeListeners();
-})
-//Swipe
-/*
-let x1 = null;
+document.addEventListener("mouseup", handleTouchEnd);
+document.addEventListener("touchend", handleTouchEnd);
 
-addSliderListeners();
+function handleTouchStart(event) {
+  isDragging = true;
+  offsetX = event.clientX + Math.abs(slides.getBoundingClientRect().left);
 
-function goRight() {
-  slides.classList.add("toRight");
-  removeSliderListeners();
-}
-
-function goLeft() {
-  slides.classList.add("toLeft");
-  removeSliderListeners();
-}
-
-function addSliderListeners() {
-  //document.addEventListener("mouseout", handleMouseOut);
-  document.addEventListener("touchstart", handleTouchStart);
-  document.addEventListener("mousedown", handleTouchStart);
-  document.addEventListener("touchmove", handleTouchMove);
   document.addEventListener("mousemove", handleTouchMove);
-}
-
-function removeSliderListeners() {
-  //document.removeEventListener("mouseout", handleMouseOut);
-  document.removeEventListener("touchstart", handleTouchStart);
-  document.removeEventListener("mousedown", handleTouchStart);
-  document.removeEventListener("touchmove", handleTouchMove);
-  document.removeEventListener("mousemove", handleTouchMove);
-}
-
-function handleTouchStart(event) {
-  x1 = event.clientX;
+  document.addEventListener("touchmove", handleTouchMove);
+  document.body.style.cursor = 'grabbing';
 }
 
 function handleTouchMove(event) {
-  if (!x1) {
-    return false;
+
+  const window = document.documentElement.clientWidth;
+
+  if (isDragging) {
+    let left = event.clientX - offsetX;
+      if(slides.getBoundingClientRect().left < (-2 * window) || slides.getBoundingClientRect().left > 0) {
+        // Make slides rearrange
+      }
+    slides.style.left = left + "px";
   }
-
-  let x2 = event.clientX;
-  console.log(x2);
-  let xDiff = x2 - x1;
-
-  if (xDiff < 0) {
-    console.log("left");
-    goLeft();
-  } else {
-    goRight();
-    console.log("right");
-  }
-  x1 = null;
-}
-
-function handleMouseOut() {
-  removeSliderListeners();
-}
-*/
-
-// ChatGPT Slider
-let startX;
-
-addListeners();
-function addListeners() {
-  document.addEventListener('mousedown', handleTouchStart);
-  document.addEventListener('touchstart', handleTouchStart);
-  document.addEventListener('mouseup', handleTouchEnd);
-  document.addEventListener('touchend', handleTouchEnd);
-}
-
-function removeListeners() {
-  document.removeEventListener('mousedown', handleTouchStart);
-  document.removeEventListener('touchstart', handleTouchStart);
-  document.removeEventListener('mousemove', handleTouchMove);
-  document.removeEventListener('touchmove', handleTouchMove);
-}
-
-function handleTouchStart(event) {
-  if (event.touches) {
-    startX = event.touches[0].clientX;
-  } else {
-    startX = event.clientX;
-  }
-
-  document.addEventListener('mousemove', handleTouchMove);
-  document.addEventListener('touchmove', handleTouchMove);
-}
-
-function handleTouchMove(event) {
-  let currentX;
-
-  if (event.touches) {
-    currentX = event.touches[0].clientX;
-  } else {
-    currentX = event.clientX;
-  }
-
-  let deltaX = currentX - startX;
-
-  if(deltaX > 0) {
-    slides.classList.add("toLeft");
-  } else {
-    slides.classList.add("toRight");
-  }
-  startX = currentX;
 }
 
 function handleTouchEnd() {
-  document.removeEventListener('mousemove', handleTouchMove);
-  document.removeEventListener('touchmove', handleTouchMove);
+  isDragging = false;
+  document.body.style.cursor = 'grab';
 }
