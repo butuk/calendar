5f5879d0e7e0b3a10a3c8f2bd4d8ce4fc4a4a4ae
Vitali Yanusheuski <bootook@gmail.com>
2025-01-02 20:47:49 +0100

    fix: fixed the bug with centering the calendar

--- js/InterfaceHeader.js
+++ js/InterfaceHeader.js
@@ -26,7 +26,7 @@ export class InterfaceHeader {
       tr.append(this.languageBlock);
     }
     const button = createElement("div", "button");
-    button.innerHTML = "▢";
+    button.innerHTML = "⃞";
     tr.append(button);
     button.addEventListener("click", (event) => {
       new InterfacePopup(version, visualization);
@@ -34,110 +34,3 @@ export class InterfaceHeader {
     place.append(tr);
   }
 }
-/*
-
-  showOverlay(version, visualization) {
-    this.overlay = createElement("div", "overlay");
-    const popup = createElement("div", "popup");
-    // Options
-    this.createOptionsLine(
-      "country",
-      visualization.object.country,
-      interfaceElements.countries,
-      popup,
-      version,
-      visualization,
-    );
-    this.createOptionsLine(
-      "language",
-      visualization.object.language,
-      interfaceElements.languages,
-      popup,
-      version,
-      visualization,
-    );
-    // Links
-    const line = createElement("div", "options-line");
-    for (let link of interfaceElements.links) {
-      const block = createElement("a", "option");
-      block.innerHTML = link.text;
-      block.href = link.url;
-      block.target = "_blank";
-      line.append(block);
-    }
-    popup.append(line);
-
-    this.overlay.prepend(popup);
-    document.body.prepend(this.overlay);
-    this.overlay.addEventListener("click", this.hideOverlay);
-    popup.addEventListener("click", this.stopPropagation);
-  }
-
-  createOptionsLine(
-    parameter,
-    setParameter,
-    optionsGroup,
-    block,
-    version,
-    visualization,
-  ) {
-    const line = createElement("div", "options-line");
-    Object.keys(optionsGroup).forEach((key) => {
-      const country = optionsGroup[key];
-      const block = createElement("div", "option");
-      block.dataset[`${parameter}`] = key;
-      if (setParameter === key) {
-        block.classList.add("option_active");
-      }
-      block.innerHTML = country;
-      const datasetKey = Object.keys(block.dataset)[0];
-      block.addEventListener("click", (event) => {
-        this.switchParameter(
-          event,
-          datasetKey,
-          block.dataset[`${parameter}`],
-          version,
-          visualization,
-        );
-      });
-      line.append(block);
-    });
-    block.append(line);
-  }
-
-  stopPropagation = (event) => {
-    event.stopPropagation();
-  };
-
-  hideOverlay() {
-    this.overlay.classList.add("overlay_fade");
-
-    this.overlay.addEventListener("transitionend", () => {
-      this.overlay.remove();
-    });
-  }
-
-  switchParameter(event, key, parameter, version, visualization) {
-    const block = event.target;
-    const optionsLine = block.parentElement;
-    for (let element of optionsLine.children) {
-      element.classList.remove("option_active");
-    }
-    block.classList.add("option_active");
-
-    switch (key) {
-      case "country":
-        version.localize(parameter);
-        localStorage.setItem("country", parameter);
-        this.countryBlock.innerHTML = interfaceElements.countries[parameter];
-        break;
-      case "language":
-        version.translate(parameter);
-        localStorage.setItem("language", parameter);
-        this.languageBlock.innerHTML = interfaceElements.languages[parameter];
-        break;
-    }
-    visualization.render(version);
-  }
-}
-*/
